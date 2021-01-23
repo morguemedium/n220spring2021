1f3e1b2b3d15a496c47c6adaee00b46546d7f50c
morguemedium <77863305+morguemedium@users.noreply.github.com>
2021-01-23 14:12:50 -0500

    testing shapes

--- labs/lab01/js/app.js
+++ labs/lab01/js/app.js
@@ -1,5 +1,22 @@
 function setup() {
     createCanvas(800,600)
 
-    circle(30,30,50)
+var black = "#000000";
+var dark = "#2A2B2B";
+var lighter = "#4E4F4F";
+var lightest = "#6D6E6E";
+
+background(black);
+
+circle(100,400,60,60);
+
+fill (lightest);
+circle(300,400,60,60);
+
+fill (lighter);
+circle(500,400,60,60);
+
+fill(dark);
+circle(700,400,60,60);
+
 }
