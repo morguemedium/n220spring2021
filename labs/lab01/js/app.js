49c8bfab5b4b29140c52eadde33390fbd7c427b8
morguemedium <77863305+morguemedium@users.noreply.github.com>
2021-01-23 15:12:39 -0500

    more circles

--- labs/lab01/js/app.js
+++ labs/lab01/js/app.js
@@ -4,19 +4,43 @@ function setup() {
 var black = "#000000";
 var dark = "#2A2B2B";
 var lighter = "#4E4F4F";
-var lightest = "#6D6E6E";
+var lightest = "#B8B8B8";
+var white = "#FFFFFF";
 
 background(black);
 
-circle(100,400,60,60);
+circle(100,500,60,60);
 
 fill (lightest);
-circle(300,400,60,60);
+circle(300,500,60,60);
 
 fill (lighter);
-circle(500,400,60,60);
+circle(500,500,60,60);
 
 fill(dark);
-circle(700,400,60,60);
+circle(700,500,60,60);
 
+fill(white);
+circle(100,330,60,60);
+
+fill(lightest);
+circle(300,330,60,60);
+
+fill(lighter);
+circle(500,330,60,60);
+
+fill(dark);
+circle(700,330,60,60);
+
+fill(white);
+circle(100,130,60,60);
+
+fill(lightest);
+circle(300,130,60,60);
+
+fill(lighter);
+circle(500,130,60,60);
+
+fill(dark);
+circle(700,130,60,60);
 }
