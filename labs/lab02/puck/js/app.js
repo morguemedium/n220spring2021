3a6c6620acfede82818280e3ee98af4642e1fd92
morguemedium <77863305+morguemedium@users.noreply.github.com>
2021-02-03 15:57:27 -0500

    puck and world

--- labs/lab02/puck/js/app.js
+++ labs/lab02/puck/js/app.js
@@ -1,13 +1,14 @@
 function setup(){
-    createCanvas(400,300)
+    createCanvas(400,300);
+    background(0);
 }
 
 //draw a circle where the mouse is at
 //p5 variables are mouseX and mouseY
-//rightt side of canvas draw red circle
+//right side of canvas draw red circle
 //left side of canvas draw blue circle
 
 function draw(){
-    background("#DEA6FF");
-    line (mouseX, 0, mouseX, 300);
+    background(0);
+    circle(mouseX,mouseY,30);
 }
--- labs/lab02/world/js/app.js
+++ labs/lab02/world/js/app.js
@@ -1,3 +1,5 @@
+xPos = 100;
+
 function setup (){
     createCanvas(800,600);
     background("#7E75FF");
@@ -5,5 +7,11 @@ function setup (){
 
 function draw(){
     background("#7E75FF");
-    circle(30,30,30);
+    circle(xPos,300,30);
+
+    xPos = xPos + 10;
+
+    if(xPos > 800){
+        xPos = 0;
+    }
 }
