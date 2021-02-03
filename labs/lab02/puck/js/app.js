3331e7744f2405de40e67d812a116dd1d005d557
morguemedium <77863305+morguemedium@users.noreply.github.com>
2021-02-03 15:40:44 -0500

    testing

--- labs/lab02/puck/js/app.js
+++ labs/lab02/puck/js/app.js
@@ -1,4 +1,13 @@
 function setup(){
     createCanvas(400,300)
-    background("#A8F1FF");
+}
+
+//draw a circle where the mouse is at
+//p5 variables are mouseX and mouseY
+//rightt side of canvas draw red circle
+//left side of canvas draw blue circle
+
+function draw(){
+    background("#DEA6FF");
+    line (mouseX, 0, mouseX, 300);
 }
---
+++ labs/lab02/world wrap/js/app.js
@@ -0,0 +1,9 @@
+function setup (){
+    createCanvas(800,600);
+    background("#7E75FF");
+}
+
+function draw(){
+    background("#7E75FF");
+    circle(30,30,30);
+}
