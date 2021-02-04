1f4f69a705bd0f72fc9fa2a48c4664881755522c
morguemedium <77863305+morguemedium@users.noreply.github.com>
2021-02-04 17:11:00 -0500

    fixing links

--- labs/lab02/larger/js/app.js
+++ labs/lab02/larger/js/app.js
@@ -1,4 +1,19 @@
+rRad = 1;
+rSpeed = 1;
+
 function setup(){
-    createCanvas(600,500);
+    createCanvas(600,400);
     background("#DE3E41");
+}
+
+function draw(){
+    background("#DE3E41");
+    fill ("#FFFFFF");
+    circle(300,200,rRad);
+
+    rRad = rRad + rSpeed;
+
+    if(rRad > 200){
+        rRad = 0;
+    }
 }
