83548598e6e38825e12f4a7fa9fc0f597127af67
morguemedium <77863305+morguemedium@users.noreply.github.com>
2021-03-19 16:51:30 -0400

    oops

---
+++ labs/lab05/collision/js/app.js
@@ -0,0 +1,16 @@
+//draw moving rectangle and circle
+//ball bounce when meeting rectangle
+
+function setup(){
+    createCanvas (400,300);
+    background("#6899B0");
+}
+
+//draw a square and a circle
+
+function draw(){
+    fill("#6CEB90");
+    stroke ("#6CEB90");
+    square(20,240,340);
+    circle(30,115,20);
+}
