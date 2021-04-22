0b12050b1fad4c61284e27e5803d6bf8e4349d20
morguemedium <77863305+morguemedium@users.noreply.github.com>
2021-04-22 17:38:25 -0400

    Man.

--- labs/lab10/mixer/js/app.js
+++ labs/lab10/mixer/js/app.js
@@ -1,40 +1,73 @@
 //one div for display box
 
-let black = ["0", "0", "0"];
-
 let displayColor = document.getElementById("displayColor");
+
+let currentColor = displayColor.style.backgroundColor;
+
 let displayColorText = document.getElementById("displayColorText");
-displayColorText.innerHTML = "current color: " + black;
+displayColorText.innerHTML = "current color: " + currentColor;
 
-let redLine = document.getElementById("redLine");
-let greenLine = document.getElementById("greenLine");
-let blueLine = document.getElementById("blueLine");
-//only has the text
+document.getElementById("redOne").addEventListener("click", plusRed); 
+document.getElementById("redFive").addEventListener("click", plusRed); 
+document.getElementById("redTen").addEventListener("click", plusRed); 
 
-let redOne = document.getElementById("redOne");
-redOne = addEventListener("click", plusOne);
-let redFive = document.getElementById("redFive");
-redFive = addEventListener("click", plusFive);
-let redTen = document.getElementById("redTen");
-redTen = addEventListener("click", plusTen);
-//connecting red buttons to corresponding addition
+document.getElementById("greenOne").addEventListener("click", plusGreen); 
+document.getElementById("greenFive").addEventListener("click", plusGreen); 
+document.getElementById("greenTen").addEventListener("click", plusGreen); 
 
-let greenOne = document.getElementById("greenOne");
-greenOne = addEventListener("click", plusOne);
-let greenFive = document.getElementById("greenFive");
-greenFive = addEventListener("click", plusFive);
-let greenTen = document.getElementById("greenTen");
-greenTen = addEventListener("click", plusTen);
-//connecting green buttons to corresponding addition
+document.getElementById("blueOne").addEventListener("click", plusBlue); 
+document.getElementById("blueFive").addEventListener("click", plusBlue); 
+document.getElementById("blueTen").addEventListener("click", plusBlue);
 
-let blueOne = document.getElementById("blueOne");
-blueOne = addEventListener("click", plusOne);
-let blueFive = document.getElementById("blueFive");
-blueFive = addEventListener("click", plusFive);
-let blueTen = document.getElementById("blueTen");
-blueTen = addEventListener("click", plusTen);
-//connecting blue buttons to corresponding addition
+function plusRed(event){
 
-function plusOne(event){}
-function plusFive(event){}
-function plusTen(event){}
+    let colorArray = currentColor.substring(4).slice(0, -1).split(",");
+    let redValue = Number(colorArray[0]);
+    //code source froom stack overflow orz getting the background color as a string, removing the non-number characters, 
+    //turning the result into an array, and 
+    //taking the first (red) element from that array to manipulate it (christ)
+    
+    let value = Number(event.target.getAttribute("data-value"));
+    
+    newValue = redValue + value;
+    var newColor = "rgb(" + newValue + "," + colorArray[1] + "," + colorArray[2] + ")";
+    currentColor = newColor;
+   
+    displayColor.style.backgroundColor = newColor;
+
+    displayColorText.innerHTML = "current color: " + newColor;
+};
+
+function plusGreen(event){
+    let colorArray = currentColor.substring(4).slice(0, -1).split(",");
+
+    let greenValue = Number(colorArray[1]);
+    let value = Number(event.target.getAttribute("data-value"));
+
+    newValue = greenValue + value;
+
+    var newColor = "rgb(" + colorArray[0] + "," + newValue + "," + colorArray[2] + ")";
+
+    displayColor.style.backgroundColor = newColor;
+    currentColor = newColor;
+
+    displayColorText.innerHTML = "current color: " + newColor;
+}
+function plusBlue(event){
+    let colorArray = currentColor.substring(4).slice(0, -1).split(",");
+
+    let blueValue = Number(colorArray[2]);
+    let value = Number(event.target.getAttribute("data-value"));
+    
+    newValue = blueValue + value;
+
+    var newColor = "rgb(" + colorArray[0] + "," + colorArray[1] + "," + newValue + ")";
+
+    displayColor.style.backgroundColor = newColor;
+    currentColor = newColor;
+
+    displayColorText.innerHTML = "current color: " + newColor;
+}
+
+//console log data value of button clicked, the color you're adding (rgb), 
+//and current value in displayColor block
